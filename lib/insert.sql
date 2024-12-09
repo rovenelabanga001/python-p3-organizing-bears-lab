a42a4a34071daca3eab9501806d394a06898fc0f
rovenelabanga <rovenelabanga2001@gmail.com>
2024-12-09 22:13:21 +0300

    sql CRUD

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex,color, temperament,alive) VALUES ('Mr. Chocolate',5,'M','Brown','idk', TRUE);
+INSERT INTO bears(name, age, sex,color, temperament,alive) VALUES ('Rowdy',6,'F','Brown','idk', TRUE);
+INSERT INTO bears(name, age, sex,color, temperament,alive) VALUES ('Tabitha',2,'M','Brown','idk', TRUE);
+INSERT INTO bears(name, age, sex,color, temperament,alive) VALUES ('Sergeant Brown',6,'M','Brown','idk', TRUE);
+INSERT INTO bears(name, age, sex,color, temperament,alive) VALUES ('Melissa',5,'F','Black','idk', TRUE);
+INSERT INTO bears(name, age, sex,color, temperament,alive) VALUES ('Grinch',7,'M','Brown','idk', TRUE);
+INSERT INTO bears(name, age, sex,color, temperament,alive) VALUES ('Wendy',6,'F','White','idk', TRUE);
+INSERT INTO bears(name, age, sex,color, temperament,alive) VALUES (NULL,5,'M','Black','idk', TRUE);
